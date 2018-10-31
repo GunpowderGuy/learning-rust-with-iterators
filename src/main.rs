77d87c6f9d7e2930ee78e29c35ab83b6dfc6e379
diegor8 <diegorosario2014@gmail.com>
2018-10-31 04:14:10 +0000

    numeros perfectos

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 fn main() {
     println!("Hello, world!");
+    println!("{}",factor(3))
 }
 
-fn main(i :i32) -
+fn factorial(n : i64) -> i64 {
+    (1..n+1).fold(1,|x,y|x*y)
+}
+
+fn factor(n:i64) ->i64 {
+    (1..n+1).map(factorial).fold(0,|x,y|x+y)
+}
+
+fn numeroPerfecto(n:i64) -> i64 {
+    n == (1..n).filter(|x|n%x == 0).fold(|x,y|x+y)
+}
