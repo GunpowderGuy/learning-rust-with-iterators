7a61b18c2a91bd9f54c9430a829e5f25114ee535
diegor8 <diegorosario2014@gmail.com>
2018-11-01 00:51:55 +0000

    second bin

--- src/semana9.rs
+++ src/semana9.rs
@@ -1,4 +1,27 @@
-fn main(){
+const ejercicio : usize = 2;
 
-println!("  fef")
+fn main() {
+
+println!("  fef");
+}
+
+fn entero() -> usize {
+let mut input_text = String::new();
+    io::stdin()
+        .read_line(&mut input_text)
+        .expect("failed to read from stdin");
+
+    let trimmed = input_text.trim();
+    match trimmed.parse::<u32>() {
+        Ok(i) => return i ,
+        Err(..) => println!("this was not an integer: {}", trimmed),
+    };
+}
+
+fn ListaInput() -> Vec<usize> {
+    (0..entero()).map(entero).collect()
+}
+
+fn SumarMayoresA10(vector :Vec<usize> ) -> usize {
+    vector.iter().fold(0,|x,y|x+y)
 }
