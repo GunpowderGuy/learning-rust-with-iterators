178560516313df57c8e36a2b893775dfbaec196e
diegor8 <diegorosario2014@gmail.com>
2018-11-01 01:59:19 +0000

    c

--- src/semana9.rs
+++ src/semana9.rs
@@ -1,27 +1,31 @@
+use std::io;
+
 const ejercicio : usize = 2;
 
 fn main() {
-
-println!("  fef");
+println!("la respuesta es :{}",SumarMayoresA10(ListaInput()));
 }
 
-fn entero() -> usize {
-let mut input_text = String::new();
+fn entero(garbage:usize) -> usize {
+    let mut input_text = String::new();
     io::stdin()
         .read_line(&mut input_text)
         .expect("failed to read from stdin");
 
     let trimmed = input_text.trim();
-    match trimmed.parse::<u32>() {
+    match trimmed.parse::<usize>() {
         Ok(i) => return i ,
         Err(..) => println!("this was not an integer: {}", trimmed),
     };
+   let  mierror : usize = 0;
+   return mierror ;
 }
 
 fn ListaInput() -> Vec<usize> {
-    (0..entero()).map(entero).collect()
+    (0..entero(0)).map(entero).collect()
 }
 
 fn SumarMayoresA10(vector :Vec<usize> ) -> usize {
-    vector.iter().fold(0,|x,y|x+y)
+    let diez : usize = 10;
+    vector.iter().filter(|x| x >= &&diez ).fold(0,|x,y|x+y)
 }
